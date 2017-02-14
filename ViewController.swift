c60e6ba1709c90e711b8ec30916969258125f7bf
Brian Walker II <bwalkeranthony@gmail.com>
2017-02-14 15:13:53 -0800

    My first commit!

---
+++ ViewController.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  Vibe App
+//
+//  Created by Brian Walker II on 2/13/17.
+//  Copyright © 2017 Vibe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var coolLabel: UILabel!
+    
+    var tapCount = 0
+    
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+        tapCount = tapCount + 1
+        if tapCount >= 10 {
+            coolLabel.text = "You've tapped the button 10 times!"
+        }
+    }
+    
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
